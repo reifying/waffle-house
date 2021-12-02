9226018250db1d464f9529d2c975be18acdeb506
Travis Brown <91909518+reify-travis-brown@users.noreply.github.com>
2021-12-01 20:44:05 -0600

    solve 1.1

--- src/advent/day1.clj
+++ src/advent/day1.clj
@@ -3,4 +3,7 @@
 (defn deeper-count
   "When provided a list of depths, count the number of times a depth measurement 
   increases from the previous measurement."
-  [coll])
+  [coll]
+  (->> (partition 2 1 coll)
+       (filter (fn [[depth1 depth2]] (< depth1 depth2)))
+       count))
