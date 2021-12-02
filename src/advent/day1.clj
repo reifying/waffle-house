9e3f1cb31f21f11fdca5b4b69bc3a6bb6049d237
Travis Brown <91909518+reify-travis-brown@users.noreply.github.com>
2021-12-01 20:56:22 -0600

    Add red sliding count test

--- src/advent/day1.clj
+++ src/advent/day1.clj
@@ -7,3 +7,8 @@
   (->> (partition 2 1 coll)
        (filter (fn [[depth1 depth2]] (< depth1 depth2)))
        count))
+
+(defn deeper-sliding-count
+  "When provided a list of depths, count the number of times the sum of 
+  measurements in this sliding window increases from the previous sum."
+  [coll])
--- test/advent/day1_test.clj
+++ test/advent/day1_test.clj
@@ -14,3 +14,15 @@
                                269
                                260
                                263]))))
+
+(deftest deeper-sliding-count-test
+  (is (= 5 (day1/deeper-sliding-count [199
+                                       200
+                                       208
+                                       210
+                                       200
+                                       207
+                                       240
+                                       269
+                                       260
+                                       263]))))
